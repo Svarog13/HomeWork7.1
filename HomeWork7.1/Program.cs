17d5de8aba7d05b3447d97377038b90919afa392
lenovo <lenovo@DESKTOP-D0T6Q1U>
2023-10-10 22:37:33 +0300

    Add project files.

---
+++ HomeWork7.1/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Entered two numbers for diapasone: ");
+int digit1 = int.Parse(Console.ReadLine());
+int digit2 = int.Parse(Console.ReadLine());
+
+if (digit1 > digit2)
+{
+    int temp = digit1;
+    digit1 = digit2;
+    digit2 = temp;
+
+}
+Console.WriteLine("Diapason of numbers: ");
+for (int i = digit1; i < digit2; i++)
+{
+    if (i % 2 == 0)
+    {
+
+        Console.Write($"{i}, ");
+    }
+}
